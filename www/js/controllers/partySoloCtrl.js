476f5d73172291b0b71555f732adeeb4a52f85cc
LESLIE <leslie.cha@hotmail.fr>
2016-02-19 14:25:57 +0100

    Affichage de la liste des question reponse

--- www/js/controllers/partySoloCtrl.js
+++ www/js/controllers/partySoloCtrl.js
@@ -1,13 +1,19 @@
 angular.module('app.partysolo', [])
-.controller('partySoloCtrl', function($scope, $http) {
-	
+.controller('partySoloCtrl', function($scope, $http, $ionicSlideBoxDelegate) {
+
 	$scope.party = "";
 	$http.get('http://127.0.0.1:9000/getQuizSimpsons')
 	.then(function(response){
-		console.log(response);
-		$scope.party = response;
+		$scope.party = response.data;
+		console.log($scope.party);
 	},
 	function(response){
 		console.error("Echec de la requete", response.status, response);
 	})
+
+	$scope.nextSlide = function() {
+    	$ionicSlideBoxDelegate.next();
+  	}
 })
+
+
