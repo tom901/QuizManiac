caa0182c57d7e708fd683a0c8679811f7fcf1aca
tom901 <thomas.paraiso@me.com>
2016-02-22 10:07:46 +0100

    Ajout du plugin pour les toast, les plugins cordova fonctionne mais il faut obligatoirement les lancer dans le navigateur

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -1,5 +1,23 @@
 angular.module('app.home', [])
-.controller('homeCtrl', function($scope, $timeout, ionicMaterialMotion, ionicMaterialInk, URL_SERVER) {
+.controller('homeCtrl', function($scope, $timeout,$cordovaToast,$ionicPlatform,
+                                 ionicMaterialMotion, ionicMaterialInk, URL_SERVER) {
+    // var device = ionic.Platform.isWebView();
+    // console.log(device);
+    //  var deviceInformation = ionic.Platform.device();
+    // console.log(deviceInformation);
+     
+    // if(device){
+    // $ionicPlatform.ready(function() {
+    //       $cordovaToast.showShortTop('Here is a message').then(function(success) {
+    //         // success
+    //         console.log('test Toast Success');
+    //       }, function (error) {
+    //         // error
+    //         console.log('test Toast Error');
+
+    //       });
+    //     });
+    // }
 // Set Header
 /*    $scope.$parent.showHeader();
     $scope.$parent.clearFabs();*/
