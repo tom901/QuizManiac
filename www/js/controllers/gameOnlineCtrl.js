dc50584f6d1a0235595d2d297d6809e7534fd4a0
Alex <cad.maxime@gmail.com>
2016-02-23 12:53:46 +0100

    Ajout du toastr

--- www/js/controllers/gameOnlineCtrl.js
+++ www/js/controllers/gameOnlineCtrl.js
@@ -1,106 +1,88 @@
 angular.module('app.gameOnline', [])
 
 .controller('GameOnlineCtrl', function($scope, $rootScope, $state, $http,$ionicHistory,
-   $ionicSlideBoxDelegate, $timeout, ionicMaterialMotion, ionicMaterialInk,
-   QuizService, GameService) {
-    $scope.countdownTxt = 10;
-    // $scope.countdown = function() {
-    //     var time = 10; /* how long the timer runs for */
-    //     var initialOffset = '440';
-    //     var i = 1
-    //     var interval = setInterval(function() {
-    //         angular.element('.circle_animation').css('stroke-dashoffset', initialOffset-(i*(initialOffset/time)));
-    //         $scope.countdownTxt = i;
-    //         if (i == time) {
-    //             clearInterval(interval);
-    //         }
-    //         i++;  
-    //     }, 1000);
-    // }
-    // $scope.countdown();
-    
+    $ionicSlideBoxDelegate, $timeout, ionicMaterialMotion, ionicMaterialInk,
+    QuizService, GameService) {
+        $scope.countdownTxt = 10;
 
 
-	//compteur pour faire défiler les questions lors de chaque réponse
-	$scope.countQuestion  = 0;
-   QuizService.getNextQuestion($rootScope.game.id,$scope.countQuestion, function(data){
-      $scope.question = data;
-  });
+        //compteur pour faire défiler les questions lors de chaque réponse
+        $scope.countQuestion  = 0;
+        QuizService.getNextQuestion($rootScope.game.id,$scope.countQuestion, function(data){
+            $scope.question = data;
+        });
 
 
-   var refreshIntervalId = setInterval(function(){ 
-        GameService.getGameByName($rootScope.game.name);
-        if($rootScope.game.countCurrentQuestion > $scope.countQuestion){
-            QuizService.getNextQuestion($rootScope.game.id, $rootScope.game.countCurrentQuestion , function(data){
-                $scope.question = data;
-                $scope.countQuestion = $rootScope.game.countCurrentQuestion;
-            });
+        var refreshIntervalId = setInterval(function(){
+            GameService.getGameByName($rootScope.game.name);
+            if($rootScope.game.countCurrentQuestion > $scope.countQuestion){
+                QuizService.getNextQuestion($rootScope.game.id, $rootScope.game.countCurrentQuestion , function(data){
+                    $scope.question = data;
+                    $scope.countQuestion = $rootScope.game.countCurrentQuestion;
+                });
+            }
+        }, 500);
+
+        var goodAnswer = 0;
+
+        $rootScope.$ionicGoBack = function(backCount) {
+            $ionicHistory.goBack(backCount);
+            console.log('dans la fonction back');
+            $rootScope.game = null;
+            $state.go('app.newOrJoinGame');
+            clearInterval(refreshIntervalId);
+
+        };
+
+        $scope.nextSlide = function() {
+            $ionicSlideBoxDelegate.next();
         }
-    }, 500);
 
-   var goodAnswer = 0;
+        $scope.nextQuestion = function(answer){
+            if(answer.weight == 1){
+                toastr.clear()
+                toastr.success('Bonne réponse !', 'Bien joué');
+                $rootScope.pad_confirm.play();
+                $scope.countQuestion++;
+                QuizService.getNextQuestion($rootScope.game.id, $scope.countQuestion , function(data){
+                    $scope.question = data;
+                });
+                goodAnswer++;
+            }else{
+                $rootScope.jar_deny.play();
+                toastr.clear()
+                toastr.error('Bonne réponse !', 'Bien joué');
+            }
 
-   $rootScope.$ionicGoBack = function(backCount) {
-        $ionicHistory.goBack(backCount);
-        console.log('dans la fonction back');
-        $rootScope.game = null;
-        $state.go('app.newOrJoinGame');
-        clearInterval(refreshIntervalId);
+            // if($scope.countQuestion == $rootScope.quizSelected.questions.length - 1){
+            //     console.log('avant calcule ');
+            //     console.log(goodAnswer);
+            //     alert('Vous avez répondu à '+ Math.round( (goodAnswer * 100) / $rootScope.quizSelected.questions.length) +'% de bonnes réponses');
+            //     $state.go('app.home');
+            //     $scope.countQuestion  = 0;
+            //     goodAnswer = 0;
+            // }else{
+            //     $scope.countQuestion++;
+            // }
+        }
+        $scope.isExpanded = false;
 
-    };
+        // Set Motion
+        $timeout(function() {
+            ionicMaterialMotion.slideUp({
+                selector: '.slide-up'
+            });
+        }, 300);
 
-   $scope.nextSlide = function() {
-     $ionicSlideBoxDelegate.next();
- }
+        $timeout(function() {
+            ionicMaterialMotion.fadeSlideInRight({
+                startVelocity: 3000
+            });
+        }, 700);
 
- $scope.nextQuestion = function(answer){
-     if(answer.weight == 1){
-        console.log('Bonne réponse !!! ');
-        $scope.countQuestion++;
-        QuizService.getNextQuestion($rootScope.game.id, $scope.countQuestion , function(data){
-        $scope.question = data;
-      });
-        goodAnswer++;
-    }else{
-     console.log('Mauvaise Réponse !!!');
- }
+        // Set Ink
+        ionicMaterialInk.displayEffect();
 
-   // if($scope.countQuestion == $rootScope.quizSelected.questions.length - 1){
-   //     console.log('avant calcule ');
-   //     console.log(goodAnswer);
-   //     alert('Vous avez répondu à '+ Math.round( (goodAnswer * 100) / $rootScope.quizSelected.questions.length) +'% de bonnes réponses');
-   //     $state.go('app.home');
-   //     $scope.countQuestion  = 0;
-   //     goodAnswer = 0;
-   // }else{
-   //     $scope.countQuestion++;
-   // }
-}
-  	// Set Header
-/*    $scope.$parent.showHeader();
-$scope.$parent.clearFabs();*/
-$scope.isExpanded = false;
-/*    $scope.$parent.setExpanded(false);
-$scope.$parent.setHeaderFab(false);*/
+        Waves.displayEffect();
 
-    // Set Motion
-    $timeout(function() {
-        ionicMaterialMotion.slideUp({
-            selector: '.slide-up'
-        });
-    }, 300);
-
-    $timeout(function() {
-        ionicMaterialMotion.fadeSlideInRight({
-            startVelocity: 3000
-        });
-    }, 700);
-
-    // Set Ink
-    ionicMaterialInk.displayEffect();
-
-    Waves.displayEffect();
-
-})
-
-
+    })
--- www/js/controllers/waitTheOthersCtrl.js
+++ www/js/controllers/waitTheOthersCtrl.js
@@ -1,17 +1,50 @@
 angular.module('app.waitTheOthers', [])
-.controller('waitTheOthersCtrl', function($scope, $rootScope, $state, $stateParams, GameService) {
-		$rootScope.audio.pause();
-		$rootScope.WaitingTheOther.play();
-		$rootScope.WaitingTheOther.volume = 0.2;
-		$rootScope.WaitingTheOther.loop = true;
+.controller('waitTheOthersCtrl', function($scope, $rootScope, $state, $stateParams, GameService, $timeout) {
+	$scope.waitingText = "En attente des autres joueurs.";
+	$rootScope.audio.pause();
+	$rootScope.WaitingTheOther.play();
+	$rootScope.WaitingTheOther.volume = 0.5;
+	$rootScope.WaitingTheOther.loop = true;
 
-		var refreshIntervalId = setInterval(function(){
-			GameService.getGameByName($rootScope.game.name);
-			if($rootScope.game.stateGame == 0){
-				alert('Le jeu va commencer');
-				clearInterval(refreshIntervalId);
-				$rootScope.WaitingTheOther.pause();
-				$state.go('app.gameOnline');
-			}
-		}, 100);
-})
+	var refreshIntervalId = setInterval(function(){
+		GameService.getGameByName($rootScope.game.name);
+		if($rootScope.game.stateGame == 0){
+			clearInterval(refreshIntervalId);
+			$rootScope.WaitingTheOther.pause();
+			angular.element(document.querySelector('#hero-has-mask')).addClass("heroUp");
+			angular.element(document.querySelector('#answerArea')).addClass("visibleUpElement");
+			$scope.countdown();
+		}
+	}, 100);
+
+
+
+	//Adding initial value for counter
+	//counter modelimiz için ilk değer atamasını yaptık.
+	$scope.counter = 10;
+	var stopped;
+
+	//timeout function
+	//1000 milliseconds = 1 second
+	//Every second counts
+	//Cancels a task associated with the promise. As a result of this, the //promise will be resolved with a rejection.
+	$scope.countdown = function() {
+		$scope.waitingText = "Tenez vous prêts !"
+		if ($scope.counter>0) {
+			stopped = $timeout(function() {
+				console.log($scope.counter);
+				$scope.counter--;
+				$scope.countdown();
+			}, 1000);
+		}else if ($scope.counter == 0) {
+			$state.go('app.gameOnline');
+		}
+	};
+
+
+	$scope.stop = function(){
+		$timeout.cancel(stopped);
+
+	}
+
+});
